2f6f7b54260be0d9b159e13ce2af58ca8d4e6496
Clown <yetanotherclown@proton.me>
2024-09-02 17:17:22 -0400

    Remove `--watch` flag in `test.sh` for Darklua commands

--- example-project/scripts/test.sh
+++ example-project/scripts/test.sh
@@ -9,7 +9,7 @@ if [ ! -d "DevPackages" ]; then
     sh scripts/install-packages.sh
 fi
 
-darklua process --config .darklua.json --watch src/ dist/src \
-    && darklua process --config .darklua.json --watch scripts/run-tests.server.luau dist/run-tests.server.luau \
+darklua process --config .darklua.json src/ dist/src \
+    && darklua process --config .darklua.json scripts/run-tests.server.luau dist/run-tests.server.luau \
     && rojo build dev.project.json --output $OUTPUT \
     && run-in-roblox --place $OUTPUT --script dist/run-tests.server.luau
